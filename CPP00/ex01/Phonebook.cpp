f21f50ce623f7a6b0fdf39a7fb9450f27f7f19dd
tpaesch <tpaesch@student.42heilbronn.de>
2024-11-13 17:07:37 +0100

    threw one contact function out

--- CPP00/ex01/Phonebook.cpp
+++ CPP00/ex01/Phonebook.cpp
@@ -6,7 +6,7 @@
 /*   By: tpaesch <tpaesch@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 13:11:15 by tpaesch           #+#    #+#             */
-/*   Updated: 2024/11/11 19:55:42 by tpaesch          ###   ########.fr       */
+/*   Updated: 2024/11/13 17:06:26 by tpaesch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ Contact::~Contact(){
 void PhoneBook::displayContacts() {
 	for (int i = 0; i < totalContacts; i++) {
 		std::cout << "Contact " << i + 1 << ":" << std::endl;
-		contacts[i].displayContact();
+		contacts[i].displayContactInfo();
 		std::cout << std::endl;
 	}
 }
@@ -42,14 +42,6 @@ void Contact::setContact(const std::string& fn,const std::string& ln,const std::
 	nickname = nn;
 	phoneNumber = pn;
 	darkestSecret = ds;
-}
-
-void Contact::displayContact() {
-	std::cout << "First Name: " << firstName << std::endl;
-	std::cout << "Last Name: " << lastName << std::endl;
-	std::cout << "Nickname: " << nickname << std::endl;
-	std::cout << "Phone Number: " << phoneNumber << std::endl;
-	std::cout << "Darkest Secret: " << darkestSecret << std::endl;
 }
 
 void PhoneBook::addContact(Contact& contact) {
