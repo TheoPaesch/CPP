0f3fb646b68bd26db8245971203ac383722d8732
tpaesch <tpaesch@student.42heilbronn.de>
2024-11-14 12:33:57 +0100

    I think it's done. now to testing

--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tpaesch <tpaesch@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 17:42:46 by tpaesch           #+#    #+#             */
-/*   Updated: 2024/11/14 10:37:32 by tpaesch          ###   ########.fr       */
+/*   Updated: 2024/11/14 12:32:55 by tpaesch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,14 +33,30 @@ int	main(int argc, char **argv) {
 	while (true) {
 		std::cout << "Enter one of the commands - ADD,SEARCH or EXIT: ";
 		std::getline(std::cin, inputLine);
+
+		if (std::cin.eof()) {
+            std::cout << "EOF detected. Exiting program." << std::endl;
+            break;
+        }
+		
 		if (inputLine == "ADD")
+		{
 			phoneBook.addContact();
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
 		else if (inputLine == "SEARCH")
+		{
 			phoneBook.searchContacts();
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
 		else if (inputLine == "EXIT")
 			return (0);
 		else
 			std::cout << "Wrong command! Try again..." << std::endl;
 	}
+
+		
 	return (0);
 }
