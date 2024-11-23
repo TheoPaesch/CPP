6ec7131516c4176577e873e7f5064cb2722529d7
tpaesch <tpaesch@student.42heilbronn.de>
2024-11-23 17:34:51 +0100

    working so far, have to look into deallocation of "zombies"

--- CPP01/ex01/Zombie.hpp
+++ CPP01/ex01/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: tpaesch <tpaesch@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 20:17:56 by tpaesch           #+#    #+#             */
-/*   Updated: 2024/11/23 17:16:57 by tpaesch          ###   ########.fr       */
+/*   Updated: 2024/11/23 17:30:24 by tpaesch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,5 @@ public:
 private:
 	std::string	_name;
 };
-
-Zombie	*newZombie(std::string name);
-
 
 #endif
--- CPP01/ex01/zombieHorde.cpp
+++ CPP01/ex01/zombieHorde.cpp
@@ -6,26 +6,17 @@
 /*   By: tpaesch <tpaesch@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 20:20:21 by tpaesch           #+#    #+#             */
-/*   Updated: 2024/11/23 17:14:21 by tpaesch          ###   ########.fr       */
+/*   Updated: 2024/11/23 17:32:37 by tpaesch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie::Zombie() {
-}
-
-Zombie::Zombie(std::string name) : _name(name) {
-}
-
-Zombie::~Zombie() {
-}
-
-Zombie*	zombieHorde(int N, std::string name) {
-	Zombie	*zombies = new Zombie[N];
-	for (int i = 0; i < N; i++) {
-		zombies[i].setName(name);
-	}
-	return (zombies);
+Zombie* Zombie::zombieHorde(int N, std::string name) {
+    Zombie* horde = new Zombie[N];
+    for (int i = 0; i < N; i++) {
+        horde[i] = Zombie(name + std::to_string(i + 1));
+    }
+    return horde;
 }
 
