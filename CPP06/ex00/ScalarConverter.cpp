f2b47e6879cef238f433d88f445b2bed38c1b7cd
tpaesch <tpaesch@student.42heilbronn.de>
2025-01-09 16:13:33 +0100

    finished ex00 have to do some testing

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -6,28 +6,33 @@
 /*   By: tpaesch <tpaesch@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 11:38:18 by tpaesch           #+#    #+#             */
-/*   Updated: 2025/01/07 16:53:28 by tpaesch          ###   ########.fr       */
+/*   Updated: 2025/01/09 16:04:52 by tpaesch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 #include "Colors.hpp"
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <climits>
+#include <cctype>
 
-ScalarConverter::ScalarConverter(std::string const &str) : _str(str)
-{
-}
+ScalarConverter::ConversionFunction ScalarConverter::conversionFunctions[4] = {
+    &ScalarConverter::convertFromChar,
+    &ScalarConverter::convertFromInt,
+    &ScalarConverter::convertFromFloat,
+    &ScalarConverter::convertFromDouble
+};
 
-ScalarConverter::ScalarConverter(ScalarConverter const &src) : _str(src._str)
-{
-}
+ScalarConverter::ScalarConverter(std::string const &str) : _str(str) {}
 
-ScalarConverter::~ScalarConverter()
-{
-}
+ScalarConverter::ScalarConverter(ScalarConverter const &src) : _str(src._str) {}
 
-ScalarConverter &ScalarConverter::operator=(ScalarConverter const &)
-{
-	return *this;
+ScalarConverter::~ScalarConverter() {}
+
+ScalarConverter &ScalarConverter::operator=(ScalarConverter const &) {
+    return *this;
 }
 
 bool isInt(std::string const &input)
@@ -79,111 +84,133 @@ bool isFloat(std::string const &input)
 }
 
 
-
-int identifyYourself(std::string const &input)
-{
-	static const std::string exceptions[7] = {"nan", "-inff", "+inff", "-infd", "+infd", "-inf", "+inf"};
-	for (int i = 0; i < 7; i++) {
-		if (input == exceptions[i]) {
-			return i; // returns whichever exception index matched
-		}
-	}
-
-	// Check for a single-character non-digit: char
-	if (input.size() == 1 && !std::isdigit(input[0])) {
-		return 7;
-	}
-	// Check for int
-	else if (isInt(input)) {
-		return 8;
-	}
-	// Check for float
-	else if (isFloat(input)) {
-		return 9;
-	}
-	// Check for double
-	else if (isDouble(input)) {
-		return 10;
-	}
-
-	return 0; // unknown
-}
-
-void ScalarConverter::convert(std::string const &input)
-{
-	int identifyer = identifyYourself(input);
-
-
-
-}
-
 void ScalarConverter::convertFromChar(const std::string& input) {
-	char character = input[0];
-	std::cout << BRIGHT_BLUE << "char: '" << character << "'" << RESET << '\n'
-			<< BRIGHT_GREEN << "int: " << static_cast<int>(character) << RESET << '\n'
-			<< BRIGHT_YELLOW << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(character) << 'f' << RESET << '\n'
-			<< BRIGHT_CYAN << "double: " << static_cast<double>(character) << RESET << std::endl;
+    char character = input[0];
+    std::cout << BRIGHT_BLUE << "char: '" << character << "'" << RESET << '\n'
+              << BRIGHT_GREEN << "int: " << static_cast<int>(character) << RESET << '\n'
+              << BRIGHT_YELLOW << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(character) << 'f' << RESET << '\n'
+              << BRIGHT_CYAN << "double: " << static_cast<double>(character) << RESET << std::endl;
 }
 
 void ScalarConverter::convertFromDouble(const std::string& input) {
-	double dbl = std::strtod(input.c_str(), NULL);
-	std::cout << BRIGHT_BLUE << "char: " << RESET;
-	if (dbl >= 0 && dbl < 128 && std::isprint(static_cast<int>(dbl))) {
-		std::cout << "'" << static_cast<char>(dbl) << "'";
-	} else {
-		std::cout << "impossible";
-	}
-	std::cout << '\n';
+    double dbl = std::strtod(input.c_str(), NULL);
+    std::cout << BRIGHT_BLUE << "char: " << RESET;
+    if (dbl >= 0 && dbl < 128 && std::isprint(static_cast<int>(dbl))) {
+        std::cout << "'" << static_cast<char>(dbl) << "'";
+    } else {
+        std::cout << "impossible";
+    }
+    std::cout << '\n';
 
-	std::cout << BRIGHT_GREEN << "int: " << RESET;
-	if (dbl > INT_MAX || dbl < INT_MIN) {
-		std::cout << "impossible";
-	} else {
-		std::cout << static_cast<int>(dbl);
-	}
-	std::cout << '\n';
+    std::cout << BRIGHT_GREEN << "int: " << RESET;
+    if (dbl > INT_MAX || dbl < INT_MIN) {
+        std::cout << "impossible";
+    } else {
+        std::cout << static_cast<int>(dbl);
+    }
+    std::cout << '\n';
 
-	std::cout << std::fixed << std::setprecision(1)
-			<< BRIGHT_YELLOW << "float: " << static_cast<float>(dbl) << 'f' << RESET << '\n'
-			<< BRIGHT_CYAN << "double: " << dbl << RESET << std::endl;
+    std::cout << std::fixed << std::setprecision(1)
+              << BRIGHT_YELLOW << "float: " << static_cast<float>(dbl) << 'f' << RESET << '\n'
+              << BRIGHT_CYAN << "double: " << dbl << RESET << std::endl;
 }
 
 void ScalarConverter::convertFromInt(const std::string& input) {
-	int dec = std::atoi(input.c_str());
-	std::cout << BRIGHT_BLUE << "char: " << RESET;
-	if (dec >= 0 && dec < 128 && std::isprint(dec)) {
-		std::cout << "'" << static_cast<char>(dec) << "'";
-	} else {
-		std::cout << "impossible";
-	}
-	std::cout << '\n';
+    int dec = std::atoi(input.c_str());
+    std::cout << BRIGHT_BLUE << "char: " << RESET;
+    if (dec >= 0 && dec < 128 && std::isprint(dec)) {
+        std::cout << "'" << static_cast<char>(dec) << "'";
+    } else {
+        std::cout << "impossible";
+    }
+    std::cout << '\n';
 
-	std::cout << BRIGHT_GREEN << "int: " << dec << RESET << '\n';
+    std::cout << BRIGHT_GREEN << "int: " << dec << RESET << '\n';
 
-	std::cout << std::fixed << std::setprecision(1)
-			<< BRIGHT_YELLOW << "float: " << static_cast<float>(dec) << 'f' << RESET << '\n'
-			<< BRIGHT_CYAN << "double: " << static_cast<double>(dec) << RESET << std::endl;
+    std::cout << std::fixed << std::setprecision(1)
+              << BRIGHT_YELLOW << "float: " << static_cast<float>(dec) << 'f' << RESET << '\n'
+              << BRIGHT_CYAN << "double: " << static_cast<double>(dec) << RESET << std::endl;
 }
 
 void ScalarConverter::convertFromFloat(const std::string& input) {
-	float flt = static_cast<float>(std::strtod(input.c_str(), NULL));
-	std::cout << BRIGHT_BLUE << "char: " << RESET;
-	if (flt >= 0 && flt < 128 && std::isprint(static_cast<int>(flt))) {
-		std::cout << "'" << static_cast<char>(flt) << "'";
-	} else {
-		std::cout << "impossible";
-	}
-	std::cout << '\n';
+    float flt = static_cast<float>(std::strtod(input.c_str(), NULL));
+    std::cout << BRIGHT_BLUE << "char: " << RESET;
+    if (flt >= 0 && flt < 128 && std::isprint(static_cast<int>(flt))) {
+        std::cout << "'" << static_cast<char>(flt) << "'";
+    } else {
+        std::cout << "impossible";
+    }
+    std::cout << '\n';
 
-	std::cout << BRIGHT_GREEN << "int: " << RESET;
-	if (flt > INT_MAX || flt < INT_MIN) {
-		std::cout << "impossible";
-	} else {
-		std::cout << static_cast<int>(flt);
-	}
-	std::cout << '\n';
+    std::cout << BRIGHT_GREEN << "int: " << RESET;
+    if (flt > INT_MAX || flt < INT_MIN) {
+        std::cout << "impossible";
+    } else {
+        std::cout << static_cast<int>(flt);
+    }
+    std::cout << '\n';
 
-	std::cout << std::fixed << std::setprecision(1)
-			<< BRIGHT_YELLOW << "float: " << flt << 'f' << RESET << '\n'
-			<< BRIGHT_CYAN << "double: " << static_cast<double>(flt) << RESET << std::endl;
+    std::cout << std::fixed << std::setprecision(1)
+              << BRIGHT_YELLOW << "float: " << flt << 'f' << RESET << '\n'
+              << BRIGHT_CYAN << "double: " << static_cast<double>(flt) << RESET << std::endl;
+}
+
+int ScalarConverter::checkSpecialCases(const std::string& input) {
+    static const std::string exceptions[5] = {"nan", "-inf", "+inf", "-inff", "+inff"};
+    for (int i = 0; i < 5; i++) {
+        if (input == exceptions[i]) {
+            return i; // returns whichever exception index matched
+        }
+    }
+    return -1; // no match found
+}
+
+void ScalarConverter::handleSpecialCases(int index) {
+    static const std::string exceptions[5] = {"nan", "-inf", "+inf", "-inff", "+inff"};
+    std::string exception = exceptions[index];
+
+    std::cout << BRIGHT_BLUE << "char: " << RESET << "impossible\n";
+    std::cout << BRIGHT_GREEN << "int: " << RESET << "impossible\n";
+
+    if (exception == "-inff" || exception == "+inff") {
+        std::cout << BRIGHT_YELLOW << "float: " << exception << RESET << '\n';
+        std::cout << BRIGHT_CYAN << "double: " << exception.substr(0, exception.size() - 1) << RESET << std::endl;
+    } else {
+        std::cout << BRIGHT_YELLOW << "float: " << exception << 'f' << RESET << '\n';
+        std::cout << BRIGHT_CYAN << "double: " << exception << RESET << std::endl;
+    }
+}
+
+ScalarConverter::ConversionType ScalarConverter::identifyYourself(const std::string& input) {
+    // Implement your logic to identify the type of input
+    // Return CHAR, INT, FLOAT, DOUBLE, or INVALID based on the input
+    // For example:
+    if (input.size() == 1 && !std::isdigit(input[0])) {
+        return CHAR;
+    }
+    if (isInt(input)) {
+        return INT;
+    }
+    if (isFloat(input)) {
+        return FLOAT;
+    }
+    if (isDouble(input)) {
+        return DOUBLE;
+    }
+    return INVALID;
+}
+
+void ScalarConverter::convert(const std::string& input) {
+    int specialCaseIndex = checkSpecialCases(input);
+    if (specialCaseIndex != -1) {
+        handleSpecialCases(specialCaseIndex);
+        return;
+    }
+
+    ConversionType type = identifyYourself(input);
+    if (type != INVALID) {
+        (this->*conversionFunctions[type])(input);
+    } else {
+        std::cout << "Invalid input" << std::endl;
+    }
 }
--- CPP06/ex00/ScalarConverter.hpp
+++ CPP06/ex00/ScalarConverter.hpp
@@ -6,39 +6,46 @@
 /*   By: tpaesch <tpaesch@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 11:38:38 by tpaesch           #+#    #+#             */
-/*   Updated: 2025/01/07 16:05:13 by tpaesch          ###   ########.fr       */
+/*   Updated: 2025/01/09 16:05:03 by tpaesch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCALARCONVERTER_HPP
-# define SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
 
-# include <iostream>
-# include <string>
-# include <iomanip>
-# include <limits>
-# include <cmath>
+#include <string>
 
-class ScalarConverter
-{
+class ScalarConverter {
 public:
-	ScalarConverter(std::string const &str);
-	ScalarConverter(ScalarConverter const &src);
-	~ScalarConverter();
+    ScalarConverter(std::string const &str);
+    ScalarConverter(ScalarConverter const &src);
+    ~ScalarConverter();
 
-	ScalarConverter &operator=(ScalarConverter const &rhs);
+    ScalarConverter &operator=(ScalarConverter const &);
 
-	int identifyYourself(std::string const &input);
-	void convert(std::string const &input);
-	bool isInt();
-	bool isFloat();
-	bool isDouble();
-	void convertFromChar(const std::string& input);
-	void convertFromDouble(const std::string& input);
-	void convertFromInt(const std::string& input);
-	void convertFromFloat(const std::string& input);
-
+    void convert(std::string const &input);
 
 private:
-	std::string const _str;
+    std::string _str;
+
+    enum ConversionType {
+        CHAR,
+        INT,
+        FLOAT,
+        DOUBLE,
+        INVALID
+    };
+
+    ConversionType identifyYourself(std::string const &input);
+    void convertFromChar(const std::string& input);
+    void convertFromInt(const std::string& input);
+    void convertFromFloat(const std::string& input);
+    void convertFromDouble(const std::string& input);
+    int checkSpecialCases(const std::string& input);
+    void handleSpecialCases(int index);
+
+    typedef void (ScalarConverter::*ConversionFunction)(const std::string& input);
+    static ConversionFunction conversionFunctions[4];
 };
+
+#endif // SCALARCONVERTER_HPP
